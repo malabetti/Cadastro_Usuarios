bcf1e48cb025f614035577998db9022346529108
malabetti <malabetti@outlook.com>
2023-09-12 16:25:48 -0300

    add sql

---
+++ sql/banco.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE cadastro;
+
+USE cadastro;
+
+CREATE TABLE usuarios (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    idusuario VARCHAR(10) NOT NULL UNIQUE,
+    senha VARCHAR(10) NOT NULL
+);
